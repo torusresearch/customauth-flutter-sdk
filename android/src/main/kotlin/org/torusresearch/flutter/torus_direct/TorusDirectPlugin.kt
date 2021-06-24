339d0ac419c1bbb0b660d41fd1bc29d7ef128916
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-06-24 17:17:15 +0700

    Handle android async

--- android/src/main/kotlin/org/torusresearch/flutter/torus_direct/TorusDirectPlugin.kt
+++ android/src/main/kotlin/org/torusresearch/flutter/torus_direct/TorusDirectPlugin.kt
@@ -1,32 +1,58 @@
 package org.torusresearch.flutter.torus_direct
 
+import android.app.Activity
+import android.content.Context
 import android.util.Log
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import org.torusresearch.torusdirect.types.DirectSdkArgs
-import org.torusresearch.torusdirect.types.TorusNetwork
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.torusresearch.torusdirect.TorusDirectSdk
+import org.torusresearch.torusdirect.types.*
 
 /** TorusDirectPlugin */
-class TorusDirectPlugin : FlutterPlugin, MethodCallHandler {
+class TorusDirectPlugin : FlutterPlugin, ActivityAware, MethodCallHandler {
     /// The MethodChannel that will the communication between Flutter and native Android
     ///
     /// This local reference serves to register the plugin with the Flutter Engine and unregister it
     /// when the Flutter Engine is detached from the Activity
     private lateinit var channel: MethodChannel
+
+    private var activity: Activity? = null
+    private lateinit var context: Context
     private lateinit var torusDirectArgs: DirectSdkArgs
 
-    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "torus_direct")
+    override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(binding.binaryMessenger, "torus_direct")
         channel.setMethodCallHandler(this)
+        context = binding.applicationContext
     }
 
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        activity = null
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivity() {
+        activity = null
     }
 
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
@@ -44,15 +70,97 @@ class TorusDirectPlugin : FlutterPlugin, MethodCallHandler {
                 result.success(null)
             }
             "triggerLogin" -> {
-                Log.d(
-                    "${javaClass.simpleName}#triggerLogin", "" +
-                            "typeOfLogin=${call.argument<String>("typeOfLogin")}, " +
-                            "verifier=${call.argument<String>("verifier")}, " +
-                            "clientId=${call.argument<String>("clientId")}"
-                )
-                result.success(null)
+                CoroutineScope(Dispatchers.Default).launch {
+                    try {
+                        val torusDirectSdk =
+                            TorusDirectSdk(torusDirectArgs, activity ?: context)
+                        val torusResponse = torusDirectSdk.triggerLogin(
+                            SubVerifierDetails(
+                                LoginType.valueOfLabel(call.argument("typeOfLogin")),
+                                call.argument<String>("verifier"),
+                                call.argument<String>("clientId"),
+                                mapJwtParams(call.argument("jwtParams")),
+                                activity == null
+                            )
+                        ).join()
+                        Log.d(
+                            "${javaClass.simpleName}#triggerLogin",
+                            "publicAddress=${torusResponse.publicAddress}"
+                        )
+                        launch(Dispatchers.Main) {
+                            result.success(
+                                mapOf(
+                                    "publicAddress" to torusResponse.publicAddress,
+                                    "privateKey" to torusResponse.privateKey
+                                )
+                            )
+                        }
+                    } catch (e: Throwable) {
+                        Log.e(
+                            "${javaClass.simpleName}#triggerLogin", e.message ?: ""
+                        )
+                        launch(Dispatchers.Main) {
+                            result.error(e.javaClass.canonicalName, e.message, null)
+                        }
+                    }
+                }
             }
             else -> result.notImplemented()
         }
+    }
+
+    private fun mapJwtParams(jwtParams: Map<String, Any>?): Auth0ClientOptions? {
+        if (jwtParams == null || !jwtParams.containsKey("domain")) {
+            return Auth0ClientOptions.Auth0ClientOptionsBuilder("").build()
+        }
+
+        val builder = Auth0ClientOptions.Auth0ClientOptionsBuilder(jwtParams["domain"] as String)
+        if (jwtParams.containsKey("isVerifierIdCaseSensitive")) {
+            builder.setVerifierIdCaseSensitive(jwtParams["isVerifierIdCaseSensitive"] as Boolean)
+        }
+        if (jwtParams.containsKey("client_id")) {
+            builder.setClient_id(jwtParams["client_id"] as String)
+        }
+        if (jwtParams.containsKey("leeway")) {
+            builder.setLeeway(jwtParams["leeway"] as String)
+        }
+        if (jwtParams.containsKey("verifierIdField")) {
+            builder.setVerifierIdField(jwtParams["verifierIdField"] as String)
+        }
+        if (jwtParams.containsKey("display")) {
+            builder.setDisplay(Display.valueOfLabel(jwtParams["display"] as String))
+        }
+        if (jwtParams.containsKey("prompt")) {
+            builder.setPrompt(Prompt.valueOfLabel(jwtParams["prompt"] as String))
+        }
+        if (jwtParams.containsKey("max_age")) {
+            builder.setMax_age(jwtParams["max_age"] as String)
+        }
+        if (jwtParams.containsKey("ui_locales")) {
+            builder.setUi_locales(jwtParams["ui_locales"] as String)
+        }
+        if (jwtParams.containsKey("id_token_hint")) {
+            builder.setId_token_hint(jwtParams["id_token_hint"] as String)
+        }
+        if (jwtParams.containsKey("login_hint")) {
+            builder.setLogin_hint(jwtParams["login_hint"] as String)
+        }
+        if (jwtParams.containsKey("acr_values")) {
+            builder.setAcr_values(jwtParams["acr_values"] as String)
+        }
+        if (jwtParams.containsKey("scope")) {
+            builder.setScope(jwtParams["scope"] as String)
+        }
+        if (jwtParams.containsKey("audience")) {
+            builder.setAudience(jwtParams["audience"] as String)
+        }
+        if (jwtParams.containsKey("connection")) {
+            builder.setConnection(jwtParams["connection"] as String)
+        }
+        if (jwtParams.containsKey("additionalParams")) {
+            builder.setAdditionalParams(jwtParams["additionalParams"] as HashMap<String, String>)
+        }
+
+        return builder.build()
     }
 }
