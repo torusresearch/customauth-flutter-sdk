d1c8ed90e516081c3523e1b1ccecd1435cf437fe
michaellee8 <ckmichael8@gmail.com>
2021-09-03 11:12:23 +0000

    chore: reformat code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:torus_direct/torus_direct.dart';
 
--- lib/torus_direct.dart
+++ lib/torus_direct.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+
 import 'package:flutter/services.dart';
 
 enum TorusNetwork { mainnet, testnet }
