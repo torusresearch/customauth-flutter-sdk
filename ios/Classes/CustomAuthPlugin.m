e51307429eb90760e5a845b14b6e87e6b2884fd2
michaellee8 <ckmichael8@gmail.com>
2021-12-01 20:34:19 +0800

    feat: actually working ios

--- ios/Classes/CustomAuthPlugin.m
+++ ios/Classes/CustomAuthPlugin.m
@@ -1,11 +1,11 @@
 #import "CustomAuthPlugin.h"
-#if __has_include(<customauth/customauth-Swift.h>)
-#import <customauth/customauth-Swift.h>
+#if __has_include(<customauth_flutter/customauth_flutter-Swift.h>)
+#import <customauth_flutter/customauth_flutter-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "customauth-Swift.h"
+#import "customauth_flutter-Swift.h"
 #endif
 
 @implementation CustomAuthPlugin
